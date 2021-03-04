75b41e81b11bd2e2b8ff97ab6d9f0e6ebece6d31
Jorge Valdes Narvaez <jorge.vnarvaez@gmail.com>
2021-03-04 12:30:33 -0300

    inicio repositorio

---
+++ 01-vuejs/js/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const Contador = {
+    data() {
+        return {
+            contador: 0
+        }
+    },
+    mounted() {
+        setInterval(() => {
+            this.contador++;
+        }, 1000);
+    }
+}
+
+Vue.createApp(Contador).mount("#contador");
+
