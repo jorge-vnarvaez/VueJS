3c50c011c50b910050c056bdfec8320963036878
Jorge Valdes Narvaez <jorge.vnarvaez@gmail.com>
2021-05-16 08:47:57 -0400

    todo-list

---
+++ fundamentos_vue/src/App.vue
@@ -0,0 +1,160 @@
+<template>
+  <h1>Lista de tareas</h1>
+  <item-form @item-agregar="agregarItem"></item-form>
+  <ul aria-labelledby="list-summary" class="stack-large">
+    <li v-for="item in ListaItems" :key="item.id">
+      <item-lista
+        :label="item.label"
+        :done="item.done"
+        :id="item.id"
+      ></item-lista>
+    </li>
+  </ul>
+</template>
+
+<script>
+import uniqueId from "lodash.uniqueid";
+import ItemForm from "./components/ItemForm.vue";
+import ItemLista from "./components/ItemLista.vue";
+
+export default {
+  name: "App",
+  components: {
+    ItemForm,
+    ItemLista,
+  },
+  data() {
+    return {
+      ListaItems: [
+        { id: uniqueId("item-"), label: "Aprender VueJS", done: false },
+        {
+          id: uniqueId("item-"),
+          label: "Crear un nuevo proyecto con Vue CLI",
+          done: true,
+        },
+        { id: uniqueId("item-"), label: "Divertirse", done: false },
+        {
+          id: uniqueId("item-"),
+          label: "Crear una lista de tareas",
+          done: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    agregarItem(item) {
+      this.ListaItems.push({ id: uniqueId("item-"), label: item, done: false });
+    },
+  },
+};
+</script>
+
+<style>
+.btn {
+  padding: 0.8rem 1rem 0.7rem;
+  border: 0.2rem solid #4d4d4d;
+  cursor: pointer;
+  text-transform: capitalize;
+}
+.btn__danger {
+  color: #fff;
+  background-color: #ca3c3c;
+  border-color: #bd2130;
+}
+.btn__filter {
+  border-color: lightgrey;
+}
+.btn__danger:focus {
+  outline-color: #c82333;
+}
+.btn__primary {
+  color: #fff;
+  background-color: #000;
+}
+.btn-group {
+  display: flex;
+  justify-content: space-between;
+}
+.btn-group > * {
+  flex: 1 1 auto;
+}
+.btn-group > * + * {
+  margin-left: 0.8rem;
+}
+.label-wrapper {
+  margin: 0;
+  flex: 0 0 100%;
+  text-align: center;
+}
+[class*="__lg"] {
+  display: inline-block;
+  width: 100%;
+  font-size: 1.9rem;
+}
+[class*="__lg"]:not(:last-child) {
+  margin-bottom: 1rem;
+}
+@media screen and (min-width: 620px) {
+  [class*="__lg"] {
+    font-size: 2.4rem;
+  }
+}
+.visually-hidden {
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(1px 1px 1px 1px);
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: rect(1px, 1px, 1px, 1px);
+  white-space: nowrap;
+}
+[class*="stack"] > * {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.stack-small > * + * {
+  margin-top: 1.25rem;
+}
+.stack-large > * + * {
+  margin-top: 2.5rem;
+}
+@media screen and (min-width: 550px) {
+  .stack-small > * + * {
+    margin-top: 1.4rem;
+  }
+  .stack-large > * + * {
+    margin-top: 2.8rem;
+  }
+}
+/* End global styles */
+#app {
+  background: #fff;
+  margin: 2rem 0 4rem 0;
+  padding: 1rem;
+  padding-top: 0;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2.5rem 5rem 0 rgba(0, 0, 0, 0.1);
+}
+@media screen and (min-width: 550px) {
+  #app {
+    padding: 4rem;
+  }
+}
+#app > * {
+  max-width: 50rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+#app > form {
+  max-width: 100%;
+}
+#app h1 {
+  display: block;
+  min-width: 100%;
+  width: 100%;
+  text-align: center;
+  margin: 0;
+  margin-bottom: 1rem;
+}
+</style>
---
+++ fundamentos_vue/src/components/ItemForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <h2 class="label-wrapper">
+      <label for="nuevo-item" class="label__lg">¿Tienes algo por hacer?</label>
+    </h2>
+    <input
+      type="text"
+      id="nuevo-item"
+      name="nuevo-item"
+      v-model.trim.lazy="label"
+      class="input__lg"
+    />
+    <button type="submit" class="btn btn__primary btn__lg">Agregar</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      label: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.label === "") {
+        return;
+      }
+      this.$emit("item-agregar", this.label);
+      this.label = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
