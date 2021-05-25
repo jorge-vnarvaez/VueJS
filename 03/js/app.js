02f48b2d90ed1581cc253aad25d2ced3c6a0a7f9
Jorge Valdes Narvaez <jorge.vnarvaez@gmail.com>
2021-05-25 08:57:58 -0400

    fetch cat images

--- 03/js/app.js
+++ 03/js/app.js
@@ -1,4 +1,23 @@
-const planilla = Vue.createApp({});
+const planilla = Vue.createApp({
+    data() {
+        return {
+            gato: {
+                url: null
+            }
+        }
+    },
+    created: function () {
+        axios({
+            method: 'get',
+            url: 'https://aws.random.cat/meow',
+            responseType: 'json'
+        })
+            .then(response => {
+                const randomUrl = response.data;
+                this.gato.url = randomUrl.file;
+            });
+    }
+});
 
 
 const vm = planilla.mount('#planilla-section');
